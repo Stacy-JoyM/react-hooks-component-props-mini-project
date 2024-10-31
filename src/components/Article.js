02729e1bc8cd478a3a51ffb97eb738cd59c94482
Stacy-JoyM <jstacymuthoka@gmail.com>
2024-10-31 05:11:53 +0300

    props mini project

---
+++ src/components/Article.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Article({title, date="January 1, 1970",preview, minutes }) {
+  return (
+    <article>
+        <h3> {title}</h3>
+        <small>{date}<span>-{minutes} min read</span></small>
+        <p> {preview}</p>
+
+    </article>
+  )
+}
+
+export default Article
